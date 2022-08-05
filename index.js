337377639c2c283695ffd7a0bf6df7e2a97b6d14
Administrator <tranvanthanh060699@gmail.com>
2022-08-06 00:02:52 +0700

    up code

---
+++ index.js
@@ -0,0 +1,25 @@
+const express =  require("express");
+const app = express();
+const port = process.env.Port | 6789;
+
+app.listen(port,function () {
+    console.log("sever is running....");
+});
+app.set("view engine","ejs");
+app.use(express.static("public"));
+
+const mysql = require("mysql");
+const conn = mysql.createConnection({
+    host:"localhost",
+    port:"3306",
+    user:"root",
+    password:"",
+    database:"....",
+    multipleStatements: true
+
+});
+app.get("/home",function (req, res) {
+    res.render("home");
+});
+
+
