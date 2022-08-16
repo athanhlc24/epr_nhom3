d05537c7ab6c4270b34e9855b9230901fa1f3bfb
Administrator <tranvanthanh060699@gmail.com>
2022-08-16 17:57:34 +0700

    up code

--- index.js
+++ index.js
@@ -19,93 +19,76 @@ const conn = mysql.createConnection({
 
 });
 var brandList =[];
-const sql_txt = "select * from brands";
+const sql = "select * from brands";
 
-conn.query(sql_txt,function (err,data){
+conn.query(sql,function (err,data){
     if(err) res.send("Not Found 404");
     else{
         brandList = data;
     }
 });
+var carsList =[];
+const sql_car = "select * from cars";
 
-app.get("/creation",function (req, res) {
-    res.render("creation",{
-        "brandList": brandList,
-    });
+conn.query(sql_car,function (err,data){
+    if(err) res.send("Not Found 404");
+    else{
+        carsList = data;
+    }
 });
-app.get("/aboutus",function (req, res) {
-    res.render("aboutus",{
-        "brandList": brandList,
-    });
-});
-app.get("/design",function (req, res) {
-    res.render("design",{
-        "brandList": brandList,
-    });
-});
-app.get("/baohanh",function (req,res) {
-    const BrName = req.query.BrName;
-    res.render("baohanh", {
-        "brandList": brandList,
-    });
-});
+function search(e){
+    e.value
+}
 app.get("/",function (req, res) {
-        const sql_txt ="select Name,Year,Price,HotCars,Fueltype from cars inner join fueltypes on cars.FtID = fueltypes.FtID" ;
-
-        conn.query(sql_txt,function (err,data){
-            if(err) res.send("Not Found 404");
-            // res.send(data);
-            else{
-                var bestList = data;
-
-
-                res.render("home",{
-                    "brandList":brandList,
-                    "bestList":bestList,
-                });
-            }
-        })
-
-    });
-
-app.get("/warranty",function (req,res){
-    const BrName = req.query.BrName;
-    const sql_txt = "select * from brands where BrName like '"+BrName+"'";
+    const sql_txt ="select Name,Year,Price,HotCars,Fueltype from cars inner join fueltypes on cars.FtID = fueltypes.FtID" ;
 
     conn.query(sql_txt,function (err,data){
         if(err) res.send("Not Found 404");
         // res.send(data);
         else{
-            var brandWarrantyList = data;
+            var bestList = data;
 
-            res.render("warranty",{
-            "brandWarrantyList":brandWarrantyList,
+
+            res.render("home",{
                 "brandList":brandList,
+                "bestList":bestList,
             });
         }
-    });
+    })
 
 });
-app.get("/phutung",function (req,res){
+
+app.get("/creation",function (req,res){
     const BrName = req.query.BrName;
-    const sql_txt = "select * from brands where BrName like '"+BrName+"'";
+    res.render("creation",{
+        "brandList":brandList,
+    });
+});
+app.get("/aboutus",function (req,res){
+    const BrName = req.query.BrName;
+    res.render("aboutus",{
+        "brandList":brandList,
+    });
+});
 
-    conn.query(sql_txt,function (err,data){
-        if(err) res.send("Not Found 404");
-        // res.send(data);
-        else{
-            var brandWarrantyList = data;
+app.get("/design",function (req,res){
+    const BrName = req.query.BrName;
+    res.render("design",{
+        "brandList":brandList,
+    });
+});
 
-            res.render("phutung",{
-                "brandWarrantyList":brandWarrantyList,
-                "brandList":brandList,
-            });
-        }
+app.get("/baohanh",function (req,res){
+    const BrName = req.query.BrName;
+    res.render("baohanh",{
+        "brandList":brandList,
     });
 
 });
+
 app.get("/list-product",function (req, res) {
     const BrName = req.query.BrName;
+    const search = req.query.search
     const sql_list ="select * from cars where BrID in(select BrID from brands where BrName like '"+BrName+"');"+
         "select BodyStyle from bodystyles inner join cars on bodystyles.BdID = cars.BdID where BrID in(select BrID from brands where BrName like '"+BrName+"');"+
         "select Fueltype from fueltypes inner join  cars on fueltypes.FtID = cars.FtID where BrID in(select BrID from brands where BrName like '"+BrName+"')";
@@ -133,11 +116,25 @@ app.get("/car-price-list",function (req, res) {
         else{
             var carPriceList = data;
             res.render("car-price-list",{
-                "carPriceList": carPriceList
+                "carPriceList": carPriceList,
+                "brandList":brandList,
             })
         }
 
     })
 });
-
-
+app.get("/search",function(req,res) {
+    const search = req.query.search
+    const sql_search = "select * from cars inner join brands on cars.BrID=brands.BrID inner join bodystyles on cars.BdID=bodystyles.BdID inner join fueltypes  on cars.FtID=fueltypes.FtID where Name like '%"+search+"%' or BodyStyle like '%"+search+"%' or Fueltype like '%"+search+"%' or BrName like '%"+search+"%' or Year like '"+search+"' ";
+    // res.send(sql_search)
+    conn.query(sql_search, function (err, data) {
+        if (err) res.send("404 Not Found");
+        else {
+            var searchList = data;
+            res.render("search", {
+                "searchList": searchList,
+                "brandList":brandList,
+            })
+        }
+    })
+});
